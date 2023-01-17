139c6ec89dc94743aaeb658e630a601f753ed5be
Callum McLuskey <callummcluskey100@gmail.com>
2023-01-17 11:04:12 +0000

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@
 # This will grab an .env file from the root directory config folder
 docker-compose --env-file ~/config/.env up --build -d 
 cat ~/config/.env > .env
+car ~/config/.frontend.env > client/src/.env
