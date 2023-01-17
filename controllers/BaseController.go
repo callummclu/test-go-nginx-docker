9a99f3abda10ac51234d33d5e04bc9df91049b6f
Callum McLuskey <callummcluskey100@gmail.com>
2023-01-17 18:03:33 +0000

    added basis for custom 404

--- controllers/BaseController.go
+++ controllers/BaseController.go
@@ -10,5 +10,10 @@ var Router *gin.Engine = gin.Default()
 func BaseController() {
 	AuthController()
 	UserController()
+	Router.LoadHTMLGlob("error/*")
+
+	Router.NoRoute(func(c *gin.Context) {
+		c.HTML(404, "404.html", gin.H{})
+	})
 	Router.Run(configs.EnvPORT())
 }
