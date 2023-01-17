6128e2ea32f860ee546c595dee4682b9d4043f93
Callum McLuskey <callummcluskey100@gmail.com>
2023-01-17 18:07:06 +0000

    removed custom 404

--- controllers/BaseController.go
+++ controllers/BaseController.go
@@ -10,10 +10,5 @@ var Router *gin.Engine = gin.Default()
 func BaseController() {
 	AuthController()
 	UserController()
-	Router.LoadHTMLGlob("error/*")
-
-	Router.NoRoute(func(c *gin.Context) {
-		c.HTML(404, "404.html", gin.H{})
-	})
 	Router.Run(configs.EnvPORT())
 }
