0f0a2841af765d8a336c5bb851eba2c1215ba153
Callum McLuskey <callummcluskey100@gmail.com>
2023-01-17 17:46:24 +0000

    updated run script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# This will grab an .env file from the root directory config folder
+# gather new changes
+git pull
+
+# move env files into repo
 cat ~/config/.env > .env
 cat ~/config/.frontend.env > client/.env
 
+# This will grab an .env file from the root directory config folder
 docker-compose --env-file ~/config/.env up --build -d 
