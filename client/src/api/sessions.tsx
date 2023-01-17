590696c2c8a24f3a0c9c9dc694b18d55e30c4fda
Callum McLuskey <callummcluskey100@gmail.com>
2023-01-17 15:20:59 +0000

    minor change to console.log

--- client/src/api/sessions.tsx
+++ client/src/api/sessions.tsx
@@ -1,6 +1,7 @@
 import { LogInUser } from "../types/auth"
 
 export async function login(loginParams: LogInUser){
+    console.log(process.env.REACT_APP_BACKEND_URI)
     const response = fetch(`${process.env.REACT_APP_BACKEND_URI}/auth/login`,{
         method:"POST",body:JSON.stringify(loginParams)
     })
