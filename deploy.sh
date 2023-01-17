322d2a68ae7d11cc6234ea7acd0e9f31e1e7768c
Callum McLuskey <callummcluskey100@gmail.com>
2023-01-17 21:12:08 +0000

    nginx ssl work

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,8 @@ git pull
 cat ~/config/.env > .env
 cat ~/config/.frontend.env > client/.env
 
+cat ~/callummclu.co.uk_ssl_certificate.cer > /etc/nginx/ssl/bundle.cer
+cat ~/'_.callummclu.co.uk_private_key. (5)key' > /etc/nginx/ssl/domain_private_key.key
+
 # This will grab an .env file from the root directory config folder
 docker-compose --env-file ~/config/.env up --build -d 
