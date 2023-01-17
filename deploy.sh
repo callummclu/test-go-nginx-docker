06d5b1e4a0f8abcb44d491c3b77629770cadd38c
Callum McLuskey <callummcluskey100@gmail.com>
2023-01-17 11:09:28 +0000

    added go module on

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This will grab an .env file from the root directory config folder
-export GO111MODULE="on"
+export GO111MODULE=on
 docker-compose --env-file ~/config/.env up --build -d 
 cat ~/config/.env > .env
 car ~/config/.frontend.env > client/src/.env
