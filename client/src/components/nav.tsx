fcc5d9a2e505266ab5ad8c65a8706857a4c7bd8e
Callum McLuskey <callummcluskey100@gmail.com>
2023-01-20 17:08:49 +0000

    fix: further navbar changes

--- client/src/components/nav.tsx
+++ client/src/components/nav.tsx
@@ -1,15 +1,23 @@
 import { Anchor, UnstyledButton } from '@mantine/core'
 import '../styles/sass/nav.scss'
 import { BsGithub,BsLinkedin } from 'react-icons/bs'
+import { BiMenu } from 'react-icons/bi'
+import { useState } from 'react'
 
 export const Nav = () => {
+
+    const [menuOpen,setMenuOpen] = useState(false)
+
+    const openMenu = () => setMenuOpen(!menuOpen)
+
     return (
-        <div className='nav'>
-            <div className='section'>
+        <div className={`nav ${menuOpen && 'open'}`}>
+            <UnstyledButton onClick={openMenu} className='hamburger'><BiMenu color={'gray'} size={24}/></UnstyledButton>
+            <div className={`section ${menuOpen && 'show'}`}>
                 <a href="/">Home</a>
                 <a href="/about">About</a>
             </div>
-            <div className='section'>
+            <div className={`section ${menuOpen && 'show icons'}`}>
                 <Anchor href="https://github.com/callummclu">
                     <BsGithub color={'gray'} size={18}/>
                 </Anchor>
