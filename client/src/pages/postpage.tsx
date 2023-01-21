83977bcb140627769cb034b0a9f5a0cea7080833
Callum McLuskey <callummcluskey100@gmail.com>
2023-01-21 15:25:20 +0000

    added some styling

--- client/src/pages/postpage.tsx
+++ client/src/pages/postpage.tsx
@@ -23,8 +23,14 @@ export const PostPage = () => {
         <>
         <div className="page">
         <LoadingOverlay loader={<Loader color="green" />}  visible={!post} overlayBlur={2} />
-            <Title>{post?.data.title}</Title>
-            <Text mb="md">{post?.data.description}</Text>
+            <div className="image-container">
+            <div className="image" style={{backgroundImage:`url(${post?.data.image})`}}></div>
+            <div className="image-overlay"></div>
+                <div className="content">
+                <Title>{post?.data.title}</Title>
+                <Text mb="md">{post?.data.description}</Text>
+                </div>
+            </div>
             {post && <Divider variant="dashed"/>}
             <Text>
                 <ReactMarkdown children={post?.data.body}/>
