f4bb6a91de60a78a716f8c1fd79d2bb43bc489d7
Callum McLuskey <callummcluskey100@gmail.com>
2023-01-21 15:07:06 +0000

    added missing post data

--- client/src/api/posts.tsx
+++ client/src/api/posts.tsx
@@ -11,3 +11,17 @@ export async function getSinglePost(id:string | number){
     })
     return response
 }
+
+interface Post{
+    title:string;
+    description:string;
+    body:string;
+    image:string;
+}
+
+export async function createPost(post:Post){
+    const response = fetch(`${process.env.REACT_APP_BACKEND_URI}/post`,{
+        method:"POST",body:JSON.stringify(post)
+    })
+    return response
+}
