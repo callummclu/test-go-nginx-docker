844ef70619e7930d0c3397b538ac5db1cca61a9b
Callum McLuskey <callummcluskey100@gmail.com>
2023-01-27 17:00:53 +0000

    removed unused files

--- client/src/helpers/userHelper.ts
+++
@@ -1,9 +0,0 @@
-export function getUser(searchParameters: string, page: number) {
-  return fetch(
-    `${process.env.REACT_APP_BACKEND_URI}/user?searchParams=${searchParameters}&itemsPerPage=20&page=${page}`
-  );
-}
-
-export function getSingleUser(username: string) {
-  return fetch(`${process.env.REACT_APP_BACKEND_URI}/user/${username}`);
-}
