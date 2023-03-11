f424e78d0f324d038ba5538dd35a9b6826ddd91c
Callum McLuskey <callummcluskey100@gmail.com>
2023-03-11 13:21:33 +0000

    fixed no cache build

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ cat ~/config/.env > .env
 cat ~/config/.frontend.env > client/.env
 
 # This will grab an .env file from the root directory config folder
-docker-compose --env-file ~/config/.env up --build -d 
+docker-compose --env-file ~/config/.env up --build --no-cache -d 
