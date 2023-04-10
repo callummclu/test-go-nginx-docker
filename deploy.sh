59399f0f719ac04fe86d2a0d8a4a97d8fa736056
Callum McLuskey <callummcluskey100@gmail.com>
2023-04-10 20:13:08 +0100

    updated mainui name

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git pull
 
 # move env files into repo
 cat ~/config/.env > .env
-cat ~/config/.frontend.env > client/.env
+cat ~/config/.frontend.env > mainui/.env
 
 # This will grab an .env file from the root directory config folder
 docker-compose build --no-cache
