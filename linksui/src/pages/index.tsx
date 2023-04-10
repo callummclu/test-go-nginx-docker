8e50e8a0f2830508458e9fdac7a518cf922fbcdc
Callum McLuskey <callummcluskey100@gmail.com>
2023-04-10 19:04:54 +0100

    updated inprogress page

--- linksui/src/pages/index.tsx
+++ linksui/src/pages/index.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import {BiConfused} from 'react-icons/bi'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="p-4">
+    <main className="p-4 flex flex-col items-center justify-center w-screen h-screen">
       <h1 className="mb-1 text-3xl">Page under construction</h1>
       <p>visit the main site <a className='text-green-500' href="https://callummclu.co.uk">here</a></p>
     </main>
