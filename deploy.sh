40414255508d8b68f819286fe40c938afd46e07e
Callum McLuskey <callummcluskey100@gmail.com>
2023-04-12 13:44:21 +0100

    moved api into webservice

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ docker system prune -a -f
 git pull
 
 # move env files into repo
-cat ~/config/.env > .env
+cat ~/config/.env > webservice/.env
 cat ~/config/.frontend.env > mainui/.env
 
 # This will grab an .env file from the root directory config folder
