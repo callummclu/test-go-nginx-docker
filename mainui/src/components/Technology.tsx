87a45582aeeb17abade6fdf07db4ab5d62b759db
Callum McLuskey <callummcluskey100@gmail.com>
2023-05-06 18:01:12 +0100

    minor style changes to

--- mainui/src/components/Technology.tsx
+++ mainui/src/components/Technology.tsx
@@ -1,14 +1,16 @@
-import { Card, Center, Image, Stack, Text } from "@mantine/core";
+import { Card, Center, Image, Stack, Text, Title } from "@mantine/core";
 
 export const TechnologyCard = ({ name }: { name: string }) => {
   return (
-    <Card m={20} w={"clamp(250px,25%, 400px)"}>
+    <Card m={20} w={"clamp(250px,25%, 400px)"} radius="xl">
       <Stack align="center">
         <Image
           src={`https://static.callummclu.co.uk/main/${name}.webp`}
           height={75}
         ></Image>
-        <Text ta="center">{name}</Text>
+        <Title order={4} ta="center">
+          {name[0].toUpperCase() + name.substring(1)}
+        </Title>
       </Stack>
     </Card>
   );
