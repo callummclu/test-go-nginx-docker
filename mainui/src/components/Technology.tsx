e8efc5f769cc61772458272b142c4750ff41b952
Callum McLuskey <callummcluskey100@gmail.com>
2023-05-07 19:30:33 +0100

    fixed tooltip error

--- mainui/src/components/Technology.tsx
+++ mainui/src/components/Technology.tsx
@@ -1,19 +1,20 @@
-import { Card, Image, Stack, Title } from "@mantine/core";
+import { Card, Image, Stack, Tooltip } from "@mantine/core";
 
 export const TechnologyCard = ({ name }: { name: string }) => {
   return (
-    <Card m={20} w={"clamp(120px,25%, 400px)"} radius="lg" shadow="md">
-      <Stack align="center">
-        <Image
-          fit="contain"
-          src={`https://static.callummclu.co.uk/main/${name}.webp`}
-          maw={70}
-          height={75}
-        ></Image>
-        <Title order={4} ta="center" fw={400}>
-          {(name[0].toUpperCase() + name.substring(1)).replace("sharp", "#")}
-        </Title>
-      </Stack>
-    </Card>
+    <Tooltip
+      label={name[0].toUpperCase() + name.substring(1).replace("sharp", "#")}
+    >
+      <Card m={20} w={"clamp(120px,25%, 400px)"} radius="lg" shadow="md">
+        <Stack align="center">
+          <Image
+            fit="contain"
+            src={`https://static.callummclu.co.uk/main/${name}.webp`}
+            maw={70}
+            height={75}
+          ></Image>
+        </Stack>
+      </Card>
+    </Tooltip>
   );
 };
