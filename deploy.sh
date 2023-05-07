24068b62ecddf58877ae12ea28ec65207f4efb2a
Callum McLuskey <callummcluskey100@gmail.com>
2023-05-07 18:41:59 +0100

    removed if conditional from deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
+git pull
 # Clear out any cached files and images from each individual rerun deployment
 docker system prune -a -f
 
-if ["$EUID" -ne 0] ; then
-    # gather new changes
-    git pull
-
-    # move env files into repo
-    cat ~/config/.env > webservice/.env
-    cat ~/config/.frontend.env > mainui/.env
-    cat ~/config/.frontend.env > adminui/.env
-else
-    echo "Local system no need to grab"
-fi
+# move env files into repo
+cat ~/config/.env > webservice/.env
+cat ~/config/.frontend.env > mainui/.env
+cat ~/config/.frontend.env > adminui/.env
 
 # This will grab an .env file from the root directory config folder
 docker-compose build --no-cache
