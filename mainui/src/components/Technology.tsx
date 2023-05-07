6cfdee510c923abd3c7f3a723289d0a6939df12b
Callum McLuskey <callummcluskey100@gmail.com>
2023-05-07 19:14:33 +0100

    added tooltips to technologies

--- mainui/src/components/Technology.tsx
+++ mainui/src/components/Technology.tsx
@@ -1,15 +1,21 @@
-import { Card, Image, Stack } from "@mantine/core";
+import { Card, Image, Stack, Tooltip } from "@mantine/core";
 
 export const TechnologyCard = ({ name }: { name: string }) => {
   return (
     <Card m={20} w={120} radius="lg" shadow="md">
       <Stack align="center">
-        <Image
-          fit="contain"
-          src={`https://static.callummclu.co.uk/main/${name}.webp`}
-          maw={75}
-          height={75}
-        ></Image>
+        <Tooltip
+          label={
+            name[0].toUpperCase() + name.substring(1).replace("sharp", "#")
+          }
+        >
+          <Image
+            fit="contain"
+            src={`https://static.callummclu.co.uk/main/${name}.webp`}
+            maw={75}
+            height={75}
+          ></Image>
+        </Tooltip>
       </Stack>
     </Card>
   );
